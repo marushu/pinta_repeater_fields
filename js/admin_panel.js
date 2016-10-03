a2ff46574f3f2a41237208c8884f09b595389fcd
Shuhei Nishimura <nishimura@digitalcube.jp>
2016-10-03 16:21:16 +0900

    fix counter

--- js/admin_panel.js
+++ js/admin_panel.js
@@ -1,18 +1,18 @@
 ( function( $ ) {
 
-	var counter = 0;
+	$( '#acf-image_ba td:nth-child(4) input[type="text"]' ).attr( 'readonly', true );
 	$( '#acf-image_ba .acf-button' ).on( 'click', function( event ) {
 
 		var target = $( 'td:nth-child(4)' ).find( 'input[type="text"]' );
-		var addValue = '[ba_image num="' + counter + '"]';
-		$( target ).eq( counter ).attr({
+		var targetCount = $(target).length - 1;
+
+		var addValue = '[ba_image num="' + targetCount + '"]';
+		$( target ).eq( targetCount ).attr({
 			'readonly': true,
 			'value':  addValue,
 			'onclick': 'this.select()',
 			'title': '選択部分をコピー( ctrl + c )して貼り付けたい部分へペーストします。'
 		});
-
-		counter++;
 
 	});
 
